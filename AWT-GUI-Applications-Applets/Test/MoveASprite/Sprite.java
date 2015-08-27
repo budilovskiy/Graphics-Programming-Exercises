0064c874876764853a3e39feae5f09bd493adcd7
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2015-08-27 21:02:02 +0300

    Create Sprite.java

---
+++ AWT-GUI-Applications-Applets/Test/MoveASprite/Sprite.java
@@ -0,0 +1,23 @@
+package customgraphics;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Sprite {
+	int x, y, width, height;
+	Color color = Color.RED;
+	
+	public Sprite(int x, int y, int width, int height, Color color) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.color = color;
+	}
+	
+	public void paint(Graphics g) {
+		g.setColor(color);
+		g.fillRect(x, y, width, height);
+	}
+
+}
