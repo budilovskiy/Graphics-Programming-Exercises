5025e5d04da5a322a0b8ae4244536cad71c82e69
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2015-08-29 15:05:43 +0300

    Create AWTCounterAnonymousInnerClass.java

---
+++ AWT-GUI-Applications-Applets/Inner-Class---Named-and-Anonymous/AWTCounterAnonymousInnerClass.java
@@ -0,0 +1,54 @@
+/*
+ * 3.  Inner Class - Named and Anonymous
+ * Compared with the AWTCounter, the following programs 
+ * AWTCounterNamedInnerClass and AWTCounterAnonymousInnerClass use 
+ * "named inner classes" and "anonymous inner classes", respectively, 
+ * as the ActionEvent listener instead of "this" object.
+ *
+ */
+package graphics.programming.exercises;
+
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.Label;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+@SuppressWarnings("serial")
+public class AWTCounterAnonymousInnerClass extends Frame {
+
+	private TextField tfCount;
+	private int count;
+
+	public AWTCounterAnonymousInnerClass() {
+		setLayout(new FlowLayout());
+		add(new Label("Counter"));
+		tfCount = new TextField(count + "", 10);
+		tfCount.setEditable(false);
+		add(tfCount);
+
+		Button btnCount = new Button("Count");
+		add(btnCount);
+
+		btnCount.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				++count;
+				tfCount.setText(count + "");
+			}
+			
+		});
+
+		setSize(300, 75);
+		setTitle("Counter");
+		setVisible(true);
+	}
+
+	public static void main(String... args) {
+		new AWTCounterAnonymousInnerClass();
+	}
+
+}
